c4dff1a0304a0a6f97dbfadeaec69334eb4acd10
unseon <unseon@gmail.com>
2015-06-24 09:38:21 +0900

    make moment

---
+++ ExampleProject/Assets/Scripts/FFani/FFaniMoment.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FFaniMomentValue {
+	public FFaniMember member;
+	public object value;
+}
+
+public class FFaniMoment {
+	public string name = "";
+	public List<FFaniMomentValue> momentValues = new List<FFaniMomentValue>();
+}
+
+public class FFaniMomentFlow {
+	Dictionary<string, FFaniMoment> moments = new Dictionary<string, FFaniMoment>();
+
+	private FFaniMoment currentMoment;
+	private string _moment = "";
+	public string moment {
+		get {
+			return currentMoment.name;
+		}
+		set {
+			currentMoment = moments[value];
+		}
+	}
+
+	public FFaniMomentFlow() {
+		currentMoment = new FFaniMoment();
+		moments[""] = currentMoment;
+	}
+}
