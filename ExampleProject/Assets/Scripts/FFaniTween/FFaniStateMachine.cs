0e87509a50baea9c92d6c82243844edbdafee30d
flexdimension <remember0523@gmail.com>
2015-12-29 20:55:44 +0900

    make state machine

---
+++ ExampleProject/Assets/Scripts/FFaniTween/FFaniStateMachine.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class FFaniSignal {
+	private Action emit;
+
+	public void connect(Action action) {
+		emit += action;
+	}
+
+	public void disconnect(Action action) {
+		emit -= action;
+	}
+}
+
+
+public class FFaniTransition {
+	string name;
+
+	public void trigger() {
+		fromState.exit();
+		toState.enter();
+	}
+
+	public Action connectSignal;
+	public Action disconnectSignal;
+
+	public FFaniState fromState;
+	public FFaniState toState;
+
+	FFaniSignal signal;
+
+	public void setSignal(FFaniSignal signal) {
+		connectSignal = () => {
+			Debug.Log("connect signal");
+			signal.connect(trigger);
+		};
+		disconnectSignal = () => {
+			signal.disconnect(trigger);
+		};
+	}
+}
+
+
+public class FFaniState {
+	public string name;
+
+	public Action onEntered	= () => {};
+	public Action onExited = () => {};
+
+
+	public  void enter() {
+		Debug.Log("state - " + name + " entered");
+		connectTransitions();
+		onEntered();
+	}
+
+	public void exit() {
+		Debug.Log("state - " + name + " exited");
+		disconnectTransitions();
+		onExited();
+	}
+
+	private void connectTransitions() {
+		for (int i = 0; i < transitions.Count; i++) {
+			transitions[i].connectSignal();
+		}
+	}
+
+	private void disconnectTransitions() {
+		for (int i = 0; i < transitions.Count; i++) {
+			transitions[i].disconnectSignal();
+		}
+	}
+
+	List<FFaniTransition> transitions = new List<FFaniTransition>();
+
+	public void addTransition(FFaniTransition transition) {
+		transition.fromState = this;
+		transitions.Add(transition);
+	}
+		
+}
+
+public class FFaniStateMachine : FFaniState{
+}
---
+++ ExampleProject/Assets/Scripts/FFaniTween/StateMachineTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class StateMachineTest : MonoBehaviour {
+
+	FFaniSignal nextAction;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Awake() {
+		state1.name = "state1";
+		state2.name = "state2";
+			
+
+		FFaniTransition trans01 = new FFaniTransition();
+
+		state1.addTransition(trans01);
+		trans01.setAction(next);
+		trans01.toState = state2;
+
+		state1.enter();
+
+		Debug.Log(next);
+	}
+
+	public Action next;
+
+	public FFaniState state1 = new FFaniState();
+	public FFaniState state2 = new FFaniState();
+
+	public void onButtonClicked() {
+		next();
+	}
+}
