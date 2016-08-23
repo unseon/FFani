229e35f5b81eacac20a184bd9023adfb5a9fd771
unseon <unseon@gmail.com>
2016-08-23 18:35:02 +0900

    make features about AnimationController

---
+++ ExampleProject/Assets/Scripts/FFaniTween/ActionTestMain.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionTestMain : MonoBehaviour {
+
+	MecanimAction maction;
+
+	void Awake () {
+		maction = GameObject.Find("unitychan").GetComponent<MecanimAction>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnJump() {
+	}
+}
---
+++ ExampleProject/Assets/UnityChan/Scripts/MecanimAction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MecanimAction : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+
+		animator.Play("WAIT00");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log(animator.GetCurrentAnimatorStateInfo(0).IsName("JUMP00"));
+		Debug.Log(animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f);
+
+//		Debug.Log("" + animator.GetCurrentAnimatorStateInfo(0).length + ", " + 
+//			animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+
+
+		Debug.Log("isLoop: " + animator.GetCurrentAnimatorStateInfo(0).loop);
+
+	}
+}
